18bbc20ce2bab3ff7f1ab7999df9b4c854087fb0
Mikalai Stelmakh <mikalai.stelmakh@gmail.com>
2023-02-14 18:43:00 +0100

    Update database for tests

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -2,9 +2,26 @@ from typing import Generator
 
 import pytest
 
-from app.database import Session
+from app.database import Base
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
-@pytest.fixture(scope="session")
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+TestingSessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+
+@pytest.fixture(scope="function")
 def db() -> Generator:
-    yield Session()
+    Base.metadata.create_all(bind=engine)
+    yield TestingSessionLocal()
+    Base.metadata.drop_all(bind=engine)
